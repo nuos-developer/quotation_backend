8e9de58cf7b77374c1570e0e2a3f3fb7e4b25387
Rohit Metakar <rohitmetakar@Rohits-MacBook-Air.local>
2026-07-30 13:15:48 +0530

    packages on cleint

--- models/adminModel.js
+++ models/adminModel.js
@@ -336,7 +336,7 @@ const dbModel = {
 
           FROM clients c
           INNER JOIN users u ON c.user_id = u.id
-          INNER JOIN roles r ON r.id = u.role_id WHERE c.deleted_by is NULL ORDER BY u.id DESC`;
+          INNER JOIN roles r ON r.id = u.role_id WHERE c.deleted_by is NULL ORDER BY c.first_name ASC`;
 
       const result = await pool.query(query);
 
